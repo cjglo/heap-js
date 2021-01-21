aff3b88083158901f4ef93848d27f28dbb9827d3
cjglo <chris.j.gallo3@gmail.com>
2021-01-21 14:24:03 -0500

    bubbleUp and Adding to heap works perfectly.

--- heap.js
+++ heap.js
@@ -4,6 +4,15 @@
 function HeapNode(value, ind) {
 	this.val = value;
 	this.index = ind;
+	this.getParent = function() {
+		return Math.floor(this.index / 2);
+	}
+	this.getLeftChild = function() {
+		return this.index * 2;
+	}
+	this.getRightChild = function() {
+		return this.index * 2 + 1;
+	}
 }
 
 // Define all the Heap Tree Functions here:
@@ -13,14 +22,14 @@ removeMin = function() {
 add = function(val) {
 		// adding an object when empty (first spot in array is a 'sentinel node' 
 		// since using the typical array rep of tree)
-		if(this.tree.length === 1) {
+		if(this.tree.length <= 1) {
 			this.tree[0] = null; // sentinel
 			this.tree[1] = new HeapNode(val, 1); // first node
 			this.last = this.tree[1];
 		}
 		// For tree that already has nodes:
 		else {
-			this.tree[this.tree.length] = new HeapNode(val, 1);
+			this.tree[this.tree.length] = new HeapNode(val, this.tree.length);
 			this.last = this.tree[this.tree.length - 1];
 			this.bubbleUp() // need to use bubbleUp to maintain structure, not so above with only 1 element, obviously
 		}
@@ -30,24 +39,27 @@ find = function(value) {
 
 }
 
-bubbleUp = function {
-		if (this.tree.last.val < this.getParent() ) // should getParent() be a heapnode method or a tree method?
+bubbleUp = function() {
+		// bubble up the min node until if is at a place that its parent is min or it is at the root
+		while(this.last.index !== 1 && this.last.val < this.tree[this.last.getParent()].val) {
+			// first swap the values
+			let temp = this.last.val;
+			this.tree[this.last.index].val = this.tree[this.last.getParent()].val;
+			this.tree[this.last.getParent()].val = temp;
+
+			// then you must change last so the while loop follows that node (really its following the value) up the tree
+			this.last = this.tree[this.last.getParent()];
+		}
+		// now must set last to the actual last value
+		this.last = this.tree[this.tree.length - 1];
 }
 
-bubbleDown = function {
+bubbleDown = function() {
 
 } 
 
-getLeftChild = function(node) {
-
-}
-
-getRightChild = function(node) {
-		
-}
-
-getParent = function(node) {
-		
+logThis = function() {
+	console.log(this);
 }
 
 // HEAP STRUCTURE
@@ -59,9 +71,6 @@ const Heap = {
 	removeMin: removeMin, 
 	add: add,
 	find: find,
-	getLeftChild: getLeftChild,
-	getRightChild: getRightChild, 
-	getParent: getParent,
 	bubbleUp: bubbleUp,
 	bubbleDown: bubbleDown,
 
@@ -73,10 +82,7 @@ const Heap = {
 		}
 	},
 
-	logThis: function() {
-		console.log("\n\n");
-		console.log(this); // just checking to make sure I didn't accidentally misassign heapnode properties to heap lol
-	},
+	logThis: logThis,
 
 }
 
@@ -84,8 +90,14 @@ const Heap = {
 // Methods to sort of "unit test" the heap data structure
 
 
-Heap.add(5);
+Heap.add(1);
+Heap.add(2);
+Heap.add(3);
+Heap.add(4);
+Heap.add(-2);
+Heap.add(-1);
+
 
 Heap.test();
 
-Heap.logThis();
+// Heap.logThis();
